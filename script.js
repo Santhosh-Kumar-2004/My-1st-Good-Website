ce04e57cf1290f702a0b01d109c3eebb5fbef35d
Santhosh <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2024-03-31 00:25:13 -0700

    Added filed via uploaded

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("The below Content is about EDITING !");
+
+var pmt = prompt("Enter your Name Below ?");
+
+var cfm = confirm("Are you want to go through");
+
+
+if(btn){
+    console.log("Ok You're Escaped...");
+}
+
+else{
+    console.log("You've been Cursed");
+}
